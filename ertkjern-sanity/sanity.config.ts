87721ff878eb2909fbd1c00f2b338a152b7974b2
ertkjern <orjanert@gmail.com>
2024-03-26 21:05:01 +0100

    Init setup

---
+++ ertkjern-sanity/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'ertkjern2.0',
+
+  projectId: 'b1kzmfwp',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
