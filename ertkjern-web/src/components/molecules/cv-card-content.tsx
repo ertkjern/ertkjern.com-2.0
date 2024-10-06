81d6a3aeffee85ee3f0be14d3a97b1b4b986c3e0
ertkjern <orjanert@gmail.com>
2024-10-06 22:26:19 +0200

    fix: date translations
    Could be solved smoother, to tired to think, and not that important

--- ertkjern-web/src/components/molecules/cv-card-content.tsx
+++ ertkjern-web/src/components/molecules/cv-card-content.tsx
@@ -1,5 +1,6 @@
 import { getFromToDate, getJobLength, getLangCode, } from "@/utils/date";
 import { format, parseISO } from "date-fns";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { FC } from "react";
 
@@ -26,9 +27,21 @@ export const CVCardContent: FC<Props> = ({
   startingText,
   locale,
 }) => {
+  const generalI18n = useTranslations('general');
+ 
   const start = parseISO(startDate ?? new Date().toISOString());
   const end = parseISO(toDate ?? new Date().toISOString());
   const hasNotStarted = start > new Date();
+
+  // Calculate job length
+  const { years, months } = getJobLength(start, end)
+  const yearString = years > 0 ? `${years} ${generalI18n('years')} ` : "";
+  const monthString =
+    months > 0 && months !== 1 ? `${months} ${generalI18n('months')} ` : "";
+
+  const jobLengthString = `${yearString}${monthString}`;
+
+
   return (
     <div className="clickable-card p-4 ">
       <div className="w-full flex justify-center align-center h-32">
@@ -36,7 +49,7 @@ export const CVCardContent: FC<Props> = ({
       </div>
       <h5 className="text-lg font-bold mt-4">{title}</h5>
       {(startDate && !hasNotStarted) && 
-        <p className="my-1 text-sm text-gray-500">{getFromToDate(start, end, locale)} <br/> {getJobLength(start, end)}</p>
+        <p className="my-1 text-sm text-gray-500">{getFromToDate(start, end, locale)} <br/> {jobLengthString}</p>
       }
       {hasNotStarted &&
         <p className="my-1 text-sm text-gray-500">{startingText}: {format(start, 'MMMM', { locale: getLangCode(locale)})}</p>
--- ertkjern-web/src/utils/date.ts
+++ ertkjern-web/src/utils/date.ts
@@ -7,32 +7,32 @@ import {
 } from "date-fns";
 import { nb, enUS } from "date-fns/locale";
 import { capitalizeFirstLetter } from "./common";
-import { useTranslations } from "next-intl";
 
-export const getJobLength = (fromDate: Date, toDate: Date): string => {
-  const generalI18n = useTranslations('general');
+interface JobLength {
+  years: number;
+  months: number;
+}
+
+export const getJobLength = (fromDate: Date, toDate: Date): JobLength  => {
   const yearDiff = differenceInYears(toDate, fromDate);
 
   // Get month diff (but not total, subtract years and add 1 as months begins on zero)
   const monthDiff = differenceInMonths(toDate, fromDate) - yearDiff * 12 + 1;
-  const yearString = yearDiff > 0 ? `${yearDiff} ${generalI18n('years')} ` : "";
-  const monthString =
-    monthDiff > 0 && monthDiff !== 1 ? `${monthDiff} ${generalI18n('months')} ` : "";
-
-  // Return string
-  return `${yearString}${monthString}`;
+  return {
+    years: yearDiff,
+    months: monthDiff,
+  }
 };
 
 export const getFromToDate = (fromDate: Date, toDate: Date, locale: string) => {
-  const generalI18n = useTranslations('general');
 
   const isStillActiveJob = isSameDay(new Date(), toDate);
-
   const langCode = getLangCode(locale);
   const from = format(fromDate, "MMMM yyyy", { locale: langCode });
   const to = format(toDate, "MMMM yyyy", { locale: langCode });
+  const translation = locale === "en" ? 'Present' : 'Pågående';
   return `${capitalizeFirstLetter(from)} - 
-            ${isStillActiveJob ? generalI18n("today") : capitalizeFirstLetter(to)}`;
+            ${isStillActiveJob ? translation : capitalizeFirstLetter(to)}`;
 };
 
 export const getLangCode = (locale: string): Locale => {
